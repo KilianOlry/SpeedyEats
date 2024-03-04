c6d76802193fdfd775e4e3f596e1356954e513de
KilianOlry <kilian.olry@gmail.com>
2024-03-04 22:34:33 +0100

    feat(variables.scss / layout.php / styles.scss / reset.scss): add variables css + link css into layout.php

--- public/views/global/layout.php
+++ public/views/global/layout.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./public/assets/css/styles.css">
   <title>SpeedyEats | KO</title>
 </head>
 <body>
